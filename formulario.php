bbe651312e03c6d49f620c457c97928af3b6f22c
g_kogan@yahoo.com.ar <64507135+guidokogan@users.noreply.github.com>
2020-04-28 22:53:32 -0300

    Traslado de Proyecto final a Repositorio

---
+++ formulario.php
@@ -0,0 +1,15 @@
+<?php
+
+$name = $_POST['nombre'];
+$email = $_POST['mail'];
+$pais = $_POST['pais'];
+$message = $_POST['mensaje'];
+
+$formcontent="De: $name \nDesde: $pais \nMensaje: $message";
+$recipient = "guidokogan@hotmail.com";
+$subject = "Contacto";
+$mailheader = "De: $email \r\n";
+
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+header('Location: enviado.html');
+?>
